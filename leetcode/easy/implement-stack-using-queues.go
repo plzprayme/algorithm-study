0dd20db922532bef89a010c6ab8e7376af3753c0
Seong Chan Hwang <34934883+plzprayme@users.noreply.github.com>
2022-05-05 18:28:39 +0900

    Create implement-stack-using-queues.go

---
+++ leetcode/easy/implement-stack-using-queues.go
@@ -0,0 +1,39 @@
+type MyStack struct {
+    stack []int
+}
+
+func Constructor() MyStack {
+    return MyStack{make([]int, 0)} 
+}
+
+
+func (this *MyStack) Push(x int)  {
+    this.stack = append(this.stack, x)
+}
+
+
+func (this *MyStack) Pop() int {
+    last := this.stack[len(this.stack) - 1]
+    this.stack = this.stack[:len(this.stack) - 1]
+    return last
+}
+
+
+func (this *MyStack) Top() int {
+    return this.stack[len(this.stack) - 1]
+}
+
+
+func (this *MyStack) Empty() bool {
+    return len(this.stack) == 0
+}
+
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
